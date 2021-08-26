8dc0547146d4bec37fc2a44d070fba55f76239d7
yousufislam <yousufislam337@gmail.com>
2021-08-26 17:50:52 +0600

    fixed navbar

--- navbar.js
+++ navbar.js
@@ -1,27 +1,12 @@
-// const nav = document.querySelector("#bar2");
-// const NavTop = nav.offsetTop;
-
-// function fixnavbar() {
-//     if (window.scrollY >= NavTop) {
-//         document.body.style.paddingTop = nav.offsetHeight + "px";
-//         document.body.classList.add("fixed-nav");
-//     } else {
-//         document.body.style.paddingTop = 0;
-//         document.body.classList.remove("fixed-nav");
-//     }
-// }
-// window.addEventListener("scroll", fixnavbar);
-
+window.onscroll = function () { myFunction() };
 
 var navbar = document.getElementById("navbar");
-
 var sticky = navbar.offsetTop;
 
-window.onscroll = function () {
-    if (window.onscrollY > sticky) {
-        navbar.classList.add("sticky");
-    }
-    else {
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
         navbar.classList.remove("sticky");
     }
-};
+}
